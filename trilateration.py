fc7fe76363f94873027fcbc88d896d3c0446becf
AidanRosen <43625295+AidanRosen@users.noreply.github.com>
2023-05-31 21:50:47 -0700

    Updated trilateration.py Working on fixing up the insert to database

--- trilateration.py
+++ trilateration.py
@@ -29,8 +29,13 @@ SOFTWARE.
 
 import os
 import numpy as np
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
 import pandas as pd
-import matplotlib.pyplot as plt
+import math 
+import sys
+#
+from heapq import nsmallest #https://stackoverflow.com/questions/22117834/how-do-i-return-a-list-of-the-3-lowest-values-in-another-list
 
 ######## Function to calculate trilateration parameters ########
 
@@ -47,202 +52,225 @@ def trilat_params(xi,yi,xj,yj,ri,rj):
 # Data to be processed is stored as csv file. 
 # Data is processed entirely using pandas dataframe
 
-def trilateration_process(path, case, ple, rssi_d0):
-	# Read csv file as panda data frame
-	df = pd.read_csv(path+case)
-
-	#AR: change this to whatever the API team hands us 
-	# Drop 'Time' column
-	df = df.drop(columns=["Time"])
-
-	# Drop NaN values
-	df = df.dropna()
-
-	# Take only column 0-3 which is AP1, AP2, and AP3
-	# This step is important as sometimes the csv data
-	# has other column aside Time, and RSSI values of 
-	# AP1, AP2, and AP3
-
-	df = df.iloc[:,0:3]
-
-	# Rename columns to AP1, AP2, and AP3
-	df.columns = ["AP1", "AP2", "AP3"]
+from dbClass import dbClass
+cse191db = dbClass()
 
 
-	# Add path loss exponent and rssi at d0 parameters
-	# Create a lambda function to convert RSSI data into distance
-	dist = lambda k, n, rssi: 10**((k - rssi) / (10*n))
+def rssiToFeet(rssi):
+		#Note: Adding our custom model for RSSI to feet 
+	return (0.0119446 * math.exp(-0.106191 * rssi))
 
-	# Add distance columns in df 
-	for i in range(1,4):
-		df["dist%d"%i] = dist(rssi_d0, ple, df["AP%d"%i])
-
-
-	# Determining what is the distance case 
-	# 1Dx means d = 1, 2Dx means d = 2, cont..
-
-	if case[0] == "1":
-		d = 1
-	elif case[0] == "2":
-		d = 2
-	elif case[0] == "3":
-		d = 3
+def getXY(deviceName):
+	match deviceName: #Largely depends on how the CSV api sends back is handled 
+		case "A":
+			return [236.1,353.54]
+		case "B":
+			return [499.2,456.35]
+		case "C":
+			return [411.93,247.5]
+		case "F":
+			return [561,330]
+		case "G":
+			return [266.3,374.7]
+		case "V":
+			return [34.64,297.9]
+		case "R":
+			return [207.3,160.3]
+		case "Q":
+			return [228.7,470.6]
+		case "P":
+			return [379.93,443.47]
+		case "L":
+			return [468.3,497.4]
+		case "I":
+			return [526.7,360.26]
+		case "K":
+			return [560.54,232.65]
+		case "T":
+			return [637.2,451.8]
+		case "X":
+			return [755.7,172.4]
+		case "M":
+			return [700,555]
+		case "H":
+			return [51.1,814.2]
+		case "W":
+			return [29.63,724.56]
+		case "E":
+			return [71.3,675.35]
+		case "O":
+			return [35.4,946.1]
+		case "U":
+			return [35.3,771.95]
+		case "D":
+			return [14,944]
+		case other:
+			return "error :("
+		
+def getFloor(deviceName):
+	if deviceName == "X" or deviceName == "M":
+		return "RIMAC 1st Floor"
+	elif deviceName == "B" or deviceName == "C" or deviceName == "F" or deviceName == "G":
+		return "RIMAC 2nd Floor" 
+	elif deviceName == "A" or deviceName == "V" or deviceName == "R" or deviceName == "Q" or deviceName == "P" or deviceName == "L" or deviceName == "I" or deviceName == "K" or deviceName == "T":
+		return "RIMAC 3rd Floor" 
+	elif deviceName == "O":
+		return "Annex Lower Floor"
+	elif deviceName == "U" or deviceName == "D" or deviceName == "N":
+		return "Annex 1st Floor"
+	elif deviceName == "H" or deviceName == "W" or deviceName == "E" or deviceName == "E" or deviceName == "E" or deviceName == "J":
+		return "Annex 2nd Floor"
 	else:
-		d = 4
+		return "error :("
 
 
-	# Setting APs coordinates
-	# AP1
-	x1 = 0
-	y1 = 0
+def trilateration_process(topDevices, rssi):
+	# # Read csv file as panda data frame
+	# df = pd.read_csv(path+case)
+	# #Note: just string concatenation
 
-	# AP2
-	x2 = 0
-	y2 = 1 * d
+	# #Note: change this to whatever the API team hands us 
+	# # Drop 'Time' column
+	# df = df.drop(columns=["Time"])
 
-	# AP3
-	x3 = 1 * d
-	y3 = 1 * d
+	# # Drop NaN values
+	# df = df.dropna()
 
-	ap_coordinates = [[x1,y1], [x2, y2], [x3, y3]]
 
-	# Calculation target Coordinates 
-	# check which STA position case is being analysed
-	# Also check whether the entered case is in capital or not
 
-	case = case.upper()
-	if case[1:3] == "D1":
-		xreal = (1/2) * d
-		yreal = 1*d
-	elif case[1:3] == "D2":
-		xreal = (1/4) * d
-		yreal = (3/4) * d
-	elif case[1:3] == "D3":
-		xreal = (1/2) * d
-		yreal = (1/2) * d
-	elif case[1:3] == "D4":
-		xreal = (1/2) * d
-		yreal = 0 * d
+	# #Start of the x-y coordinate calculating
+	# rowArray = df.iloc[i].to_numpy()
+	# #Don't want to include start index, so starting at index 1 
+	# rowArray = rowArray[1:len(rowArray)-1] #Want to exclude the end index (timestamp)
+
+	# #Now the array is ready to be iterated through
+	# rssiArray = nlargest(3, rowArray)
+
+	# topDevices = []
+	# for idx, rssi in enumerate(rssiArray):
+	# 	topDevices.append(df.columns()[rowArray.index(rssiArray[idx]) + 1])
+	# #To know which devices were the top three beacons 
+	# #Will use to triangulate
+	# #TopDevices[0] should be the closest device
+	# #Expect: an array of letters like a, b, c
+
+	# #ok so now we have our three top RSSI values and the closest device it beaconed to 
+
+	# for idx, rssi in enumerate(rssiArray):
+	# 	rssiArray[idx] = rssiToFeet(rssi)
+	# 	#Convert the rssi to feet distances
+	
+	# # Setting APs coordinates
+	# # AP1
+
+	# #
+	# #Top devices is the letternames of the devices 
+	# #AP coordinates is a list of pairs. 
+	ap_coordinates = []
+	for device in topDevices:
+		ap_coordinates.append(getXY(device))
+
+	
+	#To be returned 
+	detectedFloor = getFloor(topDevices[0])
+
+
 
 	# Trilateration parameters calculation
 	# Calculate A, B, C, D, E, and F 
-	A, B, C = trilat_params(x1, y1, x2, y2, df["dist1"], df["dist2"])
-	D, E, F = trilat_params(x2, y2, x3, y3, df["dist2"], df["dist3"])
+	A, B, C = trilat_params(ap_coordinates[0][0], ap_coordinates[0][1], ap_coordinates[1][0], ap_coordinates[1][1], rssi[0], rssi[1])
+	#Note: works because rssi[0] is the distance to the device associated with ap_coordinates[0]. True for indices 1 and 2 as well
+	D, E, F = trilat_params(ap_coordinates[1][0], ap_coordinates[1][1], ap_coordinates[2][0], ap_coordinates[2][1], rssi[1], rssi[2])
 
 	# Calculate x and y using trilateration
 	# x = CE - BF / AE - BD; y = CD - AF / BD - AE
-	xtr = (C*E - B*F) / (A*E - B*D)
-	ytr = (C*D - A*F) / (B*D - A*E)
+	if (A*E - B*D != 0):
+		xtr = (C*E - B*F) / (A*E - B*D) #A bandaid for division by zero
+	else :
+		xtr = 411 #center them by default 
+	if (B*D - A*E != 0):
+		ytr = (C*D - A*F) / (B*D - A*E)
+	else: 
+		ytr = 247
 
-	# Determine the Squared Root Error and the Mean Squared Error 
-	SQEtr = np.sqrt((xtr - xreal)**2 + (ytr - yreal)**2)
-	MSEtr = np.mean(SQEtr)
-
-	# Add target coordinates,
-	# predicted coordinates by trilateration
-	# also the SQRT error in df
-
-	df["xreal"]   = xreal  		
-	df["xtrilat"] = xtr	
-
-	df["yreal"]   = yreal
-	df["ytrilat"] = ytr
-
-	df["SQEtr"]	  = SQEtr
+#Deleting sqrt error and such, redundant.
 
 	# Return the trilateration df and MSE
-	return (df, MSEtr, ap_coordinates)
+	return [xtr, ytr, detectedFloor] #<--- need to turn these into a CSV and make a floor loop outta all dis!
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 #~~~~~~~~~~~~~~~~~~~~~~~~ Main Program ~~~~~~~~~~~~~~~~~~~~~~~~#
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 def main():
 
-	""" Run the program """
-	# Data Paths
-	# path = 'D:/Skripsweetku/Raw Data/Pengukuran Variasi Jarak Ulangan/'
-	path = 'D:/Skripsweetku/Raw Data/Pengukuran Variasi Jarak dan Manusia/'
-	cases = os.listdir(path)
-	#cases = ['3D1.csv']
 
-	# RSSI @d0 and path loss exponent (gamma)
-	gamma = 2.255
-	K = -49
+	# """ Run the program """
+	# # Data Paths
+	# # path = 'D:/Skripsweetku/Raw Data/Pengukuran Variasi Jarak Ulangan/'
+	# path = 'D:/Skripsweetku/Raw Data/Pengukuran Variasi Jarak dan Manusia/'
+	# cases = os.listdir(path)
 
-	# Run all cases in the directory
-	print(path)
-	for x in cases:
-		df, mse, ap_coords = trilateration_process(path, x, gamma, K)
+	filepath = sys.argv
+	
 
-		# Assign APs coordinates
-		ap1_xcoord = ap_coords[0][0]
-		ap1_ycoord = ap_coords[0][1]
+	df = cse191db.getDF("2023-05-15 12:30:00")
+	print(df)
+	#Note: just string concatenation
 
-		ap2_xcoord = ap_coords[1][0]
-		ap2_ycoord = ap_coords[1][1]
+	#Note: change this to whatever the API team hands us 
+	# Drop 'Time' column
+	#df = df.drop(columns=["Time"])
 
-		ap3_xcoord = ap_coords[2][0]
-		ap3_yxoord = ap_coords[2][1]
+	# Drop NaN values
+	#df = df.dropna()
 
-		#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-		""" Print All the Results """
-		print("\n")
-		print("Localization using ESP32 %s \n" %x)
-		# print(df)
-		# print(ap_coords)
-		# x_real = np.mean(df["xreal"])
-		# y_real = np.mean(df["yreal"])
-		# x_trilat = round(np.mean(df["xtrilat"]),2)
-		# y_trilat = round(np.mean(df["ytrilat"]),2)
-		# print("Real x coordinate    : ",x_real)
-		# print("Mean x Trilateration : ",x_trilat)
-		# print("Real y coordinate    : ",y_real)
-		# print("Mean y Trilateration : ",y_trilat)
-		print("MSE Trilaterasi      : ",round(mse,2))
-		# print("Standar Deviasi x-tri: ",round(np.std(df["xtrilat"]),2))
-		# print("Standar Deviasi y-tri: ",round(np.std(df["ytrilat"]),2))
+	dataFrame = {"floor":[], 'x': [], 'y': [], "timestamp":[]}
+	xyDataFrame = pd.DataFrame.from_dict(dataFrame)
+	for i in range(0, len(df.index)):
+		rowArray = df.iloc[i].to_numpy().tolist()
+		#Don't want to include start index, so starting at index 1 
+		timeStamp = rowArray[-1]
+		#print(timeStamp)
+		rssiArray = rowArray[1:len(rowArray)-1]
+		rssiArray.sort(reverse=True)
+		rssiArray = rssiArray[0:3]
+		#print(rssiArray)
 
-		# Graph limits and ticks based on case
-		if x[0] == "1":
-			limits = [-0.5, 4]
-			ticks  = np.arange(-0.5, 4.1, 0.5)
-		elif x[0] == "2":
-			limits = [-1, 3.0]
-			ticks  = np.arange(-1, 3.1, 0.5)
-		elif x[0] == "3":
-			limits = [-1, 4]
-			ticks  = np.arange(-1, 4.1, 0.5)
-		else:
-			limits = [-1, 5]
-			ticks  = np.arange(-1, 5.1, 0.5)
+		lettersOfClosest = []
+		columnsArray = df.columns.tolist()
+		copyRow = rowArray #Need this copy to pop from here as well to line up with letter row
+		for i in range(0,3):
+			foundIndex = copyRow.index(rssiArray[i])
+			lettersOfClosest.append(columnsArray[foundIndex].upper())
+			columnsArray.pop(foundIndex) #To do deal with duplicate detections
+			copyRow.pop(foundIndex)
+		
+		#print(rssiArray)
+		for idx, rssi in enumerate(rssiArray):
+			rssi = rssiToFeet(rssi) #THOUSANDS of feet of distance? Doesn't make sense. Need to check my algorithm
+			rssiArray[idx] = rssi
+		
+		#print(rssiArray)
+		#print(lettersOfClosest)
 
-		# Graphing the Results
-		# Plot the results
-		plt.figure("%s" %x[0:3], figsize = [8,6])
-		# plt.title("Trilaterasi Kasus %s\n Variasi Jarak" %x[0:3], fontsize = 15, fontweight = "bold")
-		plt.title("Trilaterasi Kasus %s\n Variasi Jarak + Manusia" %x[0:3], fontsize = 15, fontweight = "bold")
-		plt.plot(ap1_xcoord, ap1_ycoord, "X", label = "AP1", markersize = 12, c = "red")
-		plt.plot(ap2_xcoord, ap2_ycoord, "X", label = "AP2", markersize = 12, c = "navy")
-		plt.plot(ap3_xcoord, ap3_yxoord, "X", label = "AP3", markersize = 12, c = "darkgreen")
-		plt.scatter(df["xtrilat"], df["ytrilat"], label = "tri", c = "blue")
-		plt.plot(df["xreal"], df["yreal"], "D", label = "real", markersize = 9, c = "brown")
-		plt.plot(np.mean(df["xtrilat"]), np.mean(df["ytrilat"]), "*", label = "$avg_{tri}$", markersize = 12, c = "black")
+		#Now we have the two arrays we can work with. 
 
-		# Setting the limits of graph
-		# also the ticks
-		# Save the image in image directory
-		plt.ylim(limits)
-		plt.xlim(limits)
-		plt.xticks(ticks, fontsize = 12)
-		plt.yticks(ticks, fontsize = 12)
-		plt.xlabel("\nx (m)", fontsize = 12)
-		plt.ylabel("y (m)", fontsize = 12)
-		plt.legend(loc = "lower right", fontsize = 12)
-		plt.grid()
-		# plt.savefig(fname="D:/Skripsweetku/Raw Data/Grafik PNG/trilaterasi/Variasi Jarak/%s"%x[0:3], dpi=100)
-		# plt.savefig(fname="D:/Skripsweetku/Raw Data/Grafik PNG/trilaterasi/Variasi Jarak dan Manusia/%s"%x[0:3], dpi=100)
-		plt.show()
-		plt.close()
+		#Will now construct a row of x,y, and floor from the trilaterate function. Then directly insput the timestamp
+
+		newRow = trilateration_process(lettersOfClosest, rssiArray)
+		newRow.append(timeStamp)
+		#print(newRow)
+
+		cse191db.sendToDF(newRow)
+		xyDataFrame = xyDataFrame.append({"x": newRow[0], "y": newRow[1], "floor": newRow[2], "timestamp": newRow[3]}, ignore_index = True)
+	
+	#print(xyDataFrame)
+	#xyDataFrame.to_csv(r'C:\Users\aidan\Downloads\result.csv')
+	## Fix this up by turning into a responsive saved name, like taking the path of the original and adding -result to say its the result of processing the data for that day
+		
+
+		
+		
 
 if __name__ == '__main__':
 	main()
